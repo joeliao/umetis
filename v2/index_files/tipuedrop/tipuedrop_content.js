b324e3279afdfb7c8d452d71ba447ea0421dee2c
Joe Liao <lxjiulxjiu@gmail.com>
2013-10-22 13:58:59 -0700

    finsihed v2 modify

---
+++ v2/index_files/tipuedrop/tipuedrop_content.js
@@ -0,0 +1,5 @@
+var tipuedrop = {"pages": [
+     {"title": "Tipue drop", "thumb": "images/drop.png", "text": "Tipue drop, a search suggestion box jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/drop"},
+     {"title": "Tipue Search", "thumb": "images/search.png", "text": "Tipue Search, a site search engine jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/search"}, 
+     {"title": "About Tipue", "thumb": "images/about.png", "text": "Tipue is a small web development studio based in North London", "tags": "", "loc": "http://www.tipue.com/about"}
+]};
